9bb9149259bc7cca3d47d1959f90ec55401317ed
BrianAtzori <brianatzori@gmail.com>
2024-10-26 13:59:39 +0200

    Added UI Kit

--- App.tsx
+++ App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-
+import * as eva from '@eva-design/eva';
 import {Image, SafeAreaView, StyleSheet} from 'react-native';
+import {ApplicationProvider, Button, Layout, Text} from '@ui-kitten/components';
 
 function App(): React.JSX.Element {
+  const [counter, setCounter] = React.useState(0);
+
   return (
-    <SafeAreaView>
-      <Image
-        source={require('./assets/logo_task4good_png.png')}
-        style={style.logo}
-      />
-    </SafeAreaView>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <SafeAreaView>
+        <Image
+          source={require('./assets/logo_task4good_png.png')}
+          style={styles.logo}
+        />
+        <Layout style={styles.container} level="1">
+          <Button onPress={() => setCounter(counter + 1)}>BUTTON</Button>
+
+          <Text style={styles.text}>{`Pressed ${counter} times`}</Text>
+        </Layout>
+      </SafeAreaView>
+    </ApplicationProvider>
   );
 }
 
 export default App;
 
-const style = StyleSheet.create({
+const styles = StyleSheet.create({
   logo: {
     margin: 'auto',
     width: '50%',
     height: '50%',
+  },
+  container: {
+    width: '100%',
+    margin: 'auto',
+    padding: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 8,
+  },
+  text: {
+    marginHorizontal: 8,
   },
 });
