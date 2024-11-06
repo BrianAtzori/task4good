5faf8beb17ec5629248eaf8105362405cdf04bb8
BrianAtzori <brianatzori@gmail.com>
2024-11-06 22:46:26 +0100

    Improved app themeing

--- style/custom-theme-dark.ts
+++ style/custom-theme-dark.ts
@@ -2,14 +2,24 @@ import {dark} from '@eva-design/eva';
 
 export const darkTheme = {
   ...dark,
-  ...{
-    'color-primary-100': '#FFF2CC',
-    'color-primary-200': '#FFE299',
-    'color-primary-300': '#FFD166',
-    'color-primary-400': '#FFBF33',
-    'color-primary-500': '#FFAA00',
 
-    'color-success-500': '#FFAA00',
-    'color-success-600': '#CC8800',
-  },
+  'color-primary-100': '#fff7e6',
+  'color-primary-200': '#ffebb3',
+  'color-primary-300': '#ffde80',
+  'color-primary-400': '#ffd14d',
+  'color-primary-500': '#fab946',
+
+  'color-basic-100': '#191a1a',
+  'color-basic-200': '#2d2f2f',
+  'color-basic-300': '#424546',
+  'color-basic-400': '#565a5a',
+  'color-basic-800': '#191a1a',
+  'color-basic-900': '#f7f6f7',
+
+  'text-basic-color': '#f7f6f7',
+
+  'color-primary-focus': '#ffb300',
+  'color-primary-hover': '#ffcd33',
+  'color-primary-active': '#ffecb3',
+  'color-primary-disabled': '#ffe082',
 };
--- style/custom-theme-light.ts
+++ style/custom-theme-light.ts
@@ -2,14 +2,24 @@ import {light} from '@eva-design/eva';
 
 export const lightTheme = {
   ...light,
-  ...{
-    'color-primary-100': '#FFE7E7',
-    'color-primary-200': '#FFB5B5',
-    'color-primary-300': '#FF8282',
-    'color-primary-400': '#FF4F4F',
-    'color-primary-500': '#FF1C1C',
 
-    'color-success-500': '#FF1C1C',
-    'color-success-600': '#CC1818',
-  },
+  'color-primary-100': '#fff7e6',
+  'color-primary-200': '#ffebb3',
+  'color-primary-300': '#ffde80',
+  'color-primary-400': '#ffd14d',
+  'color-primary-500': '#fab946',
+
+  'color-basic-100': '#f7f6f7',
+  'color-basic-200': '#e5e4e6',
+  'color-basic-300': '#d4d3d5',
+  'color-basic-400': '#c2c1c3',
+  'color-basic-900': '#191a1a',
+
+  'text-basic-color': '#191a1a',
+  'text-control-color': '#191a1a',
+
+  'color-primary-focus': '#ffb300',
+  'color-primary-hover': '#ffcd33',
+  'color-primary-active': '#ffecb3',
+  'color-primary-disabled': '#ffe082',
 };
